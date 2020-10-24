8db823b98afcc76438b4c16bf8a35d26baa4a4ba
Anthony Grooms <anthonygroomswork@gmail.com>
2020-10-23 22:53:43 -0400

    Commit 1

--- Poacher Poacher/Assets/Scripts/GameManager.cs
+++ Poacher Poacher/Assets/Scripts/GameManager.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject poacher;
+    private int wavesLeft;
+    private int level;
+    public static readonly Vector3[] spawnPositions = new Vector3[]
+        {new Vector3(-3,-3,0), new Vector3(-1,-3,0),
+         new Vector3(1,-3,0), new Vector3(3,-3,0)};
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        level = 0;
+        StartCoroutine(NextLevel());
     }
 
-    
+    public IEnumerator NextLevel()
+    {
+        yield return new WaitForSeconds(3);
+        level++;
+        wavesLeft = (int)Mathf.Ceil(level/2f);
+
+        while (wavesLeft > 0)
+        {
+            int iMaximum = (level % 2 == 0 && wavesLeft == 1 ? 3 : 2);
+            for (int i = 1; i <= iMaximum; i++)
+                Instantiate(poacher, spawnPositions[Random.Range(0,4)],Quaternion.identity);
+            wavesLeft--;
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    public void LevelOver()
+    {
+        if (wavesLeft == 0 && !GameObject.Find("Poacher"))
+            NextLevel();
+    }
 
     // Update is called once per frame
     void Update()
---
+++ Poacher Poacher/Assets/Scripts/Poacher.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poacher : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Vector3 destinationPos;
+    private Vector3 velocity;
+    private Rigidbody2D rB;
+    public SpriteRenderer[] frames = new SpriteRenderer[2];
+    private SpriteRenderer sP;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody2D>();
+        sP = GetComponent<SpriteRenderer>();
+
+        startPos = transform.position;
+        switch (startPos.x){
+            case -3:
+                velocity = new Vector3(-4, 2, 0) - transform.position;
+                break;
+            case -1:
+                velocity = new Vector3(-1, 2, 0) - transform.position;
+                break;
+            case 1:
+                velocity = new Vector3(1, 2, 0) - transform.position;
+                break;
+            default:
+                velocity = new Vector3(4, 2, 0) - transform.position;
+                break;
+        }
+        rB.velocity = velocity.normalized;
+        StartCoroutine(PlayAnimation());
+    }
+
+    public IEnumerator PlayAnimation()
+    {
+        int frame = -1;
+        while (true)
+        {
+            frame++;
+            if (frame > frames.Length-1)
+                frame = 0;
+            sP.sprite = frames[frame].sprite;
+            yield return new WaitForSeconds(.2f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
